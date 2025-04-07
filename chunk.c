5857607a0604a54f81aa73960aeafbe5beec084a
cgarrigoza97 <31805380+cgarrigoza97@users.noreply.github.com>
2025-04-07 20:31:18 -0300

    add support for more than 255 constants

--- chunk.c
+++ chunk.c
@@ -35,3 +35,20 @@ int addConstant(Chunk* chunk, Value value) {
     writeValueArray(&chunk->constants, value);
     return chunk->constants.count - 1;
 }
+
+void writeConstant(Chunk* chunk, Value value, int line) {
+    int index = addConstant(chunk, value);
+    if (index <= 255) {
+        writeChunk(chunk, OP_CONSTANT, line);
+        writeChunk(chunk, index, line);
+        return;
+    }
+
+    writeChunk(chunk, OP_CONSTANT_LONG, line);
+    writeChunk(chunk, (index >> 16) & 0x00FF, line);
+    writeChunk(chunk, (index >> 8) & 0x00FF, line);
+    writeChunk(chunk, index & 0x00FF, line);
+    
+    return;
+
+}
--- debug.c
+++ debug.c
@@ -27,6 +27,20 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     return offset + 2;
 }
 
+static int constantLongInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t byte1 = chunk->code[offset + 1];
+    uint8_t byte2 = chunk->code[offset + 2];
+    uint8_t byte3 = chunk->code[offset + 3];
+
+    int index = ((int) byte1 << 16) | ((int) byte2 << 8) | (int) byte3;
+
+    printf("%-16s %4d '", name, index);
+    printValue(chunk->constants.values[index]);
+    printf("'\n");
+    return offset + 4;
+
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
     int lineNumber = getLine(&chunk->lines, offset);
@@ -36,6 +50,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     switch (instruction) {
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_CONSTANT_LONG:
+            return constantLongInstruction("OP_CONSTANT_LONG", chunk, offset);
         case OP_RETURN: 
             return simpleInstruction("OP_RETURN", offset);
         default:
